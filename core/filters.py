655fde80c01ba03a5c8ed37c89acc1bc554a7c9b
Osama Salem <oafsalem2000@gmail.com>
2021-12-18 22:41:06 +0300

    added more features to the search

--- core/filters.py
+++ core/filters.py
@@ -2,9 +2,10 @@ import django_filters
 from .models import Book
 
 class BookFilter(django_filters.FilterSet):
-    
-    subject = django_filters.CharFilter()
-    authors = django_filters.CharFilter()
+
+    book_title = django_filters.CharFilter(lookup_expr='icontains')
+    # subject = django_filters.Filter(lookup_expr='icontains')
+    # authors = django_filters.Filter(lookup_expr='icontains')
     publication_year = django_filters.CharFilter()
     class Meta:
         model =  Book
