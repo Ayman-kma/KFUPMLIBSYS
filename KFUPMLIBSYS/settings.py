704dcdace522bca2114d8ff0a6179099dfd6884a
Osama Salem <oafsalem2000@gmail.com>
2021-12-19 16:31:52 +0300

    added congfig files for heroku

--- KFUPMLIBSYS/settings.py
+++ KFUPMLIBSYS/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ as env
+import django_heroku
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ntb166kpkcdtq5bidd5ck4f)u(nhr%l63@!vdqr7zkakl_%%0'
+SECRET_KEY = env.get(
+    'SECRET_KEY', 'django-insecure-%ntb166kpkcdtq5bidd5ck4f)u(nhr%l63@!vdqr7zkakl_%%0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,3 +134,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# This line used to cofigure django Heroku
+django_heroku.settings(locals())
